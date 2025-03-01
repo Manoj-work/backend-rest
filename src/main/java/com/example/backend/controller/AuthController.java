68784d1ba7c36dfe195603df18bff7730fc9f8d7
Manoj-work <jdmanoj2025@gmail.com>
2025-03-01 17:23:33 +0530

    added cors

--- src/main/java/com/example/backend/controller/AuthController.java
+++ src/main/java/com/example/backend/controller/AuthController.java
@@ -5,6 +5,7 @@ import com.example.backend.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://192.168.0.200:3000")
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
@@ -12,10 +13,10 @@ public class AuthController {
 
     private final AuthService authService;
 
-    @PostMapping("/register")
-    public String register(@RequestBody RegisterRequest request) {
-        return authService.register(request);
-    }
+//    @PostMapping("/register")
+//    public String register(@RequestBody RegisterRequest request) {
+//        return authService.register(request);
+//    }
 
     @PostMapping("/login")
     public AuthResponse login(@RequestBody AuthRequest request) {
